8df7b7741fa5a11d2fe465739d99febaf53c56b5
Wembley Leach <wembley@hey.com>
2020-11-14 13:22:36 -0500

    Add support for printing the version from Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,46 @@ mod parser;
 const DALIA_CONFIG_ENV_VAR: &str = "DALIA_CONFIG_PATH";
 const CONFIG_FILE: &str = "config";
 const DEFAULT_DALIA_CONFIG_PATH: &str = "~/.dalia";
+const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 const USAGE: &str = r#"Usage: dalia <command> [arguments]
 
 Commands:
     aliases: Generates all shell aliases for each configured directory at DALIA_CONFIG_PATH
+    version: The current build version
     help: Prints this usage message
     
 Examples:
-$ dalia aliases
+    $ dalia aliases
 
 Environment:
 DALIA_CONFIG_PATH
     The location where dalia looks for alias configurations. This is set to $HOME/dalia by default.
     Put the alias configurations in a file named `config` here. 
     
-Use "dalia help <command> for more information about that command.
-"#;
+Use "dalia help <command> for more information about that command."#;
 
 const ALIASES_USAGE: &str = r#"Usage: dalia aliases
 
 Description:
-Aliases generates shell aliases for each directory listed in DALIA_CONFIG_PATH/config.
-The aliases are only for changing directories to the specified locations. No other types
-of aliases are supported.
-
-Each alias outputted by this command is of the form `alias path="cd /some/path"`.
-
-The configuration file uses its own format to generate aliases. The simplest way to generate
-an alias to a directory is to provide its absolute path on disk. The generated alias will use
-the lowercase name of directory at the end of the absolute path as the name of the alias. The
-alias name can be customized as well, by prepending the absolute path with a custom name surrounded
-by square brackets (i.e. `[` and `]`). The casing of the custom name doesn't change, so if it's
-provided in titlecase, snakecase, or any other case, the alias will be created with that case in
-tact.
-
-This command also expands a single directory into multiple aliases when the configured line starts with
-an asterisk surrounded by square brackets (i.e. `[*]`), which tells the parser to traverse the immediate
-children of the given directory and create lowercase named aliases for only the items that are directories.
-All children that are files are ignored. 
+    Aliases generates shell aliases for each directory listed in DALIA_CONFIG_PATH/config.
+    The aliases are only for changing directories to the specified locations. No other types
+    of aliases are supported.
+    
+    Each alias outputted by this command is of the form `alias path="cd /some/path"`.
+    
+    The configuration file uses its own format to generate aliases. The simplest way to generate
+    an alias to a directory is to provide its absolute path on disk. The generated alias will use
+    the lowercase name of directory at the end of the absolute path as the name of the alias. The
+    alias name can be customized as well, by prepending the absolute path with a custom name surrounded
+    by square brackets (i.e. `[` and `]`). The casing of the custom name doesn't change, so if it's
+    provided in titlecase, snakecase, or any other case, the alias will be created with that case in
+    tact.
+    
+    This command also expands a single directory into multiple aliases when the configured line starts with
+    an asterisk surrounded by square brackets (i.e. `[*]`), which tells the parser to traverse the immediate
+    children of the given directory and create lowercase named aliases for only the items that are directories.
+    All children that are files are ignored.
 
 Examples:
     Simple path
@@ -65,8 +66,12 @@ Examples:
         alias two='cd /some/path/two'
         alias three='cd /some/path/three'
         
-    when /some/path has contents /one, /two, file.txt, and /three.
-"#;
+    when /some/path has contents /one, /two, file.txt, and /three."#;
+
+const VERSION_USAGE: &str = r#"Usage: dalia version
+
+Description:
+    Version prints the current semantic version of the dalia executable."#;
 
 #[derive(Debug)]
 struct Configuration<'a> {
@@ -119,6 +124,7 @@ fn run_command(cmd: &str, args: &[String]) {
         if args.len() == 2 {
             match args[1].as_str() {
                 "aliases" => print_alias_usage(),
+                "version" => print_version_usage(),
                 _ => {
                     eprintln!("dalia: unknown command argument {}\n", args[1]);
                     print_usage();
@@ -127,6 +133,8 @@ fn run_command(cmd: &str, args: &[String]) {
         } else {
             print_usage()
         }
+    } else if "version" == cmd {
+        print_version()
     } else {
         eprintln!("dalia: unknown command {}", cmd);
         print_usage();
@@ -182,4 +190,14 @@ fn print_usage() {
 
 fn print_alias_usage() {
     println!("{}", ALIASES_USAGE)
+}
+
+fn print_version_usage() {
+    println!("{}", VERSION_USAGE)
+}
+
+fn print_version() {
+    if let Some(v) = VERSION {
+        println!("dalia version {}", v)
+    }
 }
